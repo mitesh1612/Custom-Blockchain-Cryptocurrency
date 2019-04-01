2fb1471ba8327ba9687312252e1f3311dc19f953
mitesh1612 <miteshrocker@gmail.com>
2019-04-01 20:09:03 +0000

    Added Unit Tests for Block Class

---
+++ block.test.js
@@ -0,0 +1,30 @@
+const Block = require("./block");
+
+// Describe serves as a description for the test. Second parameter is an arrow function that contains a series of tests that jest will execute once it finds a describe block function
+let data, lastBlock, block;
+describe("Block",() => {
+    //Before Each allows us to run the same code before each following unit tests 
+    // For unit tests, we use "it" function. First Parameter is Description. Second is an arrow function that has the code to execute the test
+    
+    beforeEach(() => {
+        data = "bar";
+        lastBlock = Block.genesis();
+        block = Block.mineBlock(lastBlock,data);
+    });
+
+    // These variables are limited to scope of beforeEach so we declare them before the describe and assign in the beforeEach block
+    
+    it('Sets the `data` to match the Input',() => {
+        //Expect takes one piece of data and then we can chain methods after the expect function to describe what we expect the inputted data to be
+        expect(block.data).toEqual(data);
+        // This expects that the block created where the data of the block should match the data
+    });
+    
+    it('Sets the `lastHash` to the Hast of the Last Block',() => {
+        expect(block.lastHash).toEqual(lastBlock.hash);
+        // This expects the blocks last hash to be equal to the previous block's hash
+    });
+});
+
+// Update the package.json now in scripts now. There we put 'jest --watchAll' (watchAll is similar to nodemon which restarts on changes)
+
